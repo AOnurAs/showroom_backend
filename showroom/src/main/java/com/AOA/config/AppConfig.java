7674cc4f21a16a0d8a10fb7b546a875a9c9b183e
Ali Onur Aslan <onuras0399@hotmail.com>
2025-08-07 13:47:26 +0300

    Implement AppConfig and SecurityConfig; update error structure accordingly

---
+++ showroom/src/main/java/com/AOA/config/AppConfig.java
@@ -0,0 +1,65 @@
+package com.AOA.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.AOA.exception.BaseExcepiton;
+import com.AOA.exception.ErrorMessage;
+import com.AOA.exception.MessageType;
+import com.AOA.model.User;
+import com.AOA.repository.UserRepository;
+
+@Configuration
+public class AppConfig {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+			
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				Optional<User> optional = userRepository.findByUsername(username);
+				
+				if(optional.isEmpty()) {
+					throw new BaseExcepiton(new ErrorMessage(MessageType.USERNAME_NOT_FOUND , "username couldnt be found in db"));
+					// return null
+				}
+				return optional.get();
+			}
+		};
+		
+	}
+	
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder());
+		
+		return provider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+
+}
--- showroom/src/main/java/com/AOA/exception/MessageType.java
+++ showroom/src/main/java/com/AOA/exception/MessageType.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 public enum MessageType {
 	NO_RECORD_EXIST("1004", "No record found"),
 	TOKEN_NOT_FOUND("1005", "Token coudnt be found in the request"),
-	GENERAL_EXCEPTION("9999", "An error occured"),
-	TOKEN_EXPIRED("1008", "Token expired");
+	USERNAME_NOT_FOUND("1006", "Username not found in the database"),
+	TOKEN_EXPIRED("1008", "Token expired"),
+	GENERAL_EXCEPTION("9999", "An error occured");
 	
 	private String code;
 	private String message;
